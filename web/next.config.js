8be8012b58116bb81a307149411ee0355add2ad8
Adrian le Bas <adrianleb@gmail.com>
2018-12-11 20:36:14 +0100

    cleaning up initial commit

--- web/next.config.js
+++ web/next.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-  webpack: (config) => {
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' }
+    };
+  },
+
+  webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
       fs: 'empty'
-    }
+    };
 
-    return config
+    return config;
   }
-}
+};
--- web/styles/UI.js
+++ web/styles/UI.js
@@ -1,13 +1,7 @@
 export default ({ typography, spacingBase, colors, helpers }) => {
-  // Most style presets related to the project should go here!
   const UI = {
     colors: {
       text: colors.black,
-      linkHover: colors.gray,
-      top: colors.gray,
-      headerText: colors.black,
-      byline: colors.gray,
-
       link: {
         default: colors.grayDark,
         hover: colors.black
@@ -21,13 +15,7 @@ export default ({ typography, spacingBase, colors, helpers }) => {
     },
 
     layout: {
-      topheight: spacingBase * 50, // 128
-      pageWidthMax: spacingBase * 230, // 920
-      gridHorizontalGutters: spacingBase * 10, // 16
-      gridVerticalGutters: spacingBase * 10, // 16,
-      interviewTileImageHeight: spacingBase * 60, // 120
-      introTileWidth: spacingBase * 300,
-      minTileWidth: spacingBase * 20
+      exampleValue: spacingBase * 50 // 200
     }
   };
 
