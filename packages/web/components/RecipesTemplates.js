bbfcaa0900f5fb20f3308a9ab7c7bc645c81ed6c
Phil Bain <bain.pmcg@gmail.com>
2020-04-10 13:08:33 +0200

    Textareas recipe with default variant [ch574]

--- packages/web/components/RecipesTemplates.js
+++ packages/web/components/RecipesTemplates.js
@@ -30,6 +30,32 @@ export default [
   `,
   ],
   [
+    'Textareas',
+    'Textareas with labels and error state.',
+    '#?code=N4Igxg9gJgpiBcIA8BhAhgJygPgDoDsACQpAcQwEscDjjUJ8AXNC-GDPI2ugGTQCMYAG2wAJCAHdCUCIQCeEAK6EAZjGEB+JAHo+gkTW4kAKjAAezDDDTYdpi5muduOlA2at2z2vSYs2HIYuesJiktKyCspqmjohBlwu9pbW0jAqaIpCjABqaEKKMAC8uCAAIumZ2YTJpbbayY42QSTabn6egYkk7R4B3sECoeJSMvJKqupCWrpDCUYm5ilohABumBRoTCUgrAAOioxlWwDm7HV2S00DPsmEAM5mRcDAhAC28IQADIQAvr9rDZbRg7e5vfJCY74M4YOotbgAUQwGAgGHeMHu9zQZ0IJwgGMIAAt2DB4a1kjdXO5-F5DDpyFROK5MNR8CAADQgRjEt4YhAAbRAAFk0ABrdgVDJZRggAC6nIkVG59wFAGYAExfdkAdgAbAAOWW-IA',
+    `<Card>
+    <Grid>
+      <Container>
+        <Label>How do you feel?</Label>
+        <Textarea></Textarea>
+      </Container>
+      <Container>
+        <Label>How do you feel?</Label>
+        <Textarea defaultValue="Default Text"></Textarea>
+      </Container>
+      <Container>
+        <Label>How do you feel?</Label>
+        <Textarea variant="inputDanger"></Textarea>
+        <Text sx={{ m: 0 }} variant="smallDanger">
+          Error message goes here
+        </Text>
+      </Container>
+    </Grid>
+  </Card>
+  
+  `,
+  ],
+  [
     'Sliders',
     'Sliders with labels and error state.',
     '#?code=N4Igxg9gJgpiBcIA8BhAhgJygPgDoDsACQpAcQwEspCBnADwF5hhCBzSqAJQgHdS0ADvEIAWQgF9xeIsRIoI+AC5oK+GBmmzZSADJoARjAA22AMpGq6wkYPGkAej2GTBLcSTnLGh59gbX2vbySipq-jLuwcqq6ppuurYmvlZoYIoUAG4wDk7GcVoeFn6EGZgUaEoMuCA0ReoAgmmZMNXYPnXhBUEK0WFxDuRU0g7oWNIgADQgigAWMAC2MDQIANogALJoANbqACIwAGZoAK5GiiAAulM8VLPL8CsAzABMAAwTAOwAbAAcF+JAA',
