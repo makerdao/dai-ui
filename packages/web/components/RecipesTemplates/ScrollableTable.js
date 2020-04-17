174c9bd6a2089cea80ea3b2ad60420957962413e
pacxiu <nedvedek111@gmail.com>
2020-04-17 13:40:50 +0200

    Add keys to mapped elements in ScrollableTable

--- packages/web/components/RecipesTemplates/ScrollableTable.js
+++ packages/web/components/RecipesTemplates/ScrollableTable.js
@@ -1,7 +1,7 @@
 const title = "Scrollable Table";
 const description = "Scrollable Table for grouped content";
 const playroomHash =
-  "#?code=N4Igxg9gJgpiBcIA8BhAhgJygAgM4A8BeYYbAWwE95sAmAGnP2oG17sAdENAVwBcJOAXQYAHagAZRAI2oBmBmTT4A6gEsovABbVOAVnHiR+TtgC+pgHzsAdtjvYkAMQA2MfHiIlsY7POxpnVQBzawBJXhgyXB1wGGsIjE4GACtuXF5VADMKFAh4uN4Y3BE0MBgAWikYXgB3GDiTcytbezskABU3Xg9iUky83mUYYM1CjhApCGcoJOx++IBlVQAvGGoAFjNLBYgyGGx2tClXA9VeVyQAek78XmbWtoAhPn5rCxRAsABrK+feV-u9iuLjcgLaAHEMOobA9sJBnNwyNZcMRZKYYQ8CL0MbDJlgYBhHhB-rsYgBGIx4KbQkB0HEPPGwDDtCA+TgU9y4akzWn01qMgm5ZwQDAxIIYGAUGhJPn2IzUeiyuwiKjYMl0lqtcz0sH2YDMTjtTQwNBQMmzQ3G03S2njI0mqCyIQAOkUIgAFO7NABKbCECzYd1Khw3bpYrzzQbDIKjGKTaaNSzATSmK6h3V2b3e9GahyXSHqXVIInGXN2cPAYOKfAACWjsfGAA5DMZeWX7BAAG4EzLCmoxHj8GXtuwCwnE-hkcmUrmBHka2GjkVMoUisUSqXDxfeJi0BeLlXUdXBzgAMng8HKdSkXzO5VwYAwU2cUkwnGolZH9hq6i0MV0Rhbtu5CqNYagaNo4wAa2+6wqYsEPGeF5XjAN53g+T7OC+mDlLwGClF877YJ+wHYK+3zihA3DWFAq6iuMNSaGcMBAduY4AEqmqoaRFIoWGsQ88EniA56Xtet68Pej7Pq+GC4ZoiJSERJHAeRXyUdRtFTGu4zipKNoIbiy4EpxUDcdE4y4HxzgCVqhlbDqsr6sGBogE+NTmra7IWiAECESAwguZw7kGeMnkMJwflCPZdiue5TpeSA4XjFFAUxdgcUQDU6w+clkX+YFX6ZTUui5T5qWFduxUAGxlYlFX2YIrpoB67rub6-qBsGSAFjg8KIsiqKWMGdjAO5zWtWUWEdQGQZfq0HRdBYwBTc4qbXEtI3YFmObblcvUZtt2b0r8ED4ICVzoFgFi0iAWiRDAuAIMwIAALJoF8BIACIwJkPDOLwaUgD+EFPfAzCyDQkgAOzVY2gimEAA";
+  "#?code=N4Igxg9gJgpiBcIAESA8BhAhgJykgzgB4C8wwSAtgJ7xIBMANJYbQNqNIA6ImArgC4RuAXSYAHWgAZxAI1oBmJhUyEA6gEso-ABa1uAVkmSxhbkgC+5gHycAdihSoAYgBsYhAiTJIJSRUkwXdQBzWwBJfhgKfD1wGFtI7G4mACtefH51ADMqdAgE+P5Y-DFMMBgAWhkYfgB3GHizSxt7B0cAFXd+T1JyLPz+VRgQ7SKuEBkIFyhkpH6EgGV1AC8YWgAWC2sFiAoYJHbMGTcD9X43VAB6TsJ+FrbHACEBQVsrdCCwAGsr5-5X+4OK6udyAxwAcWwmjsDyQkBcvAotnwpHk5hhDyIvQxsMmuBg2EeEH+u1iAEYTAQptCQAwcQ88bBsO0IL5uBSPPhqTNafS2oyCXkXBBsLFgtgYFQ6Mk+Q4TLRGLKUGIaEgyXTWm1LPSwQ5gKxuO1tDBMFAybNDcbTdLaeMjSaoPIRAA6ZRiAAU7u0TC+koAlEhiFYkO6lWgbt0sd55oNhsFRrFJtMmuYkL6qKR09ZgNpzFcI7qUH6-ejNWhLpDNLrUETTGWUFHgGHlIQABJxhPjAAcxlMvPrDggADcCVlhbVYnxBDKBygBYTiYIKOTKVygjyNbC5yKmUKRWKJVKZ1ufCx6JutyraOqw9wAGTweAVeoyL5nCr4MDYKYuGQ4bi0E2s4OLUmg6LE+gmMeJ6UOotgaFoujjJBfYXrC5hoQ896Ps+MCvu+n7fi4v44BU-DYGUXwAUgQEwUgf7fOKEC8LYUB7qK4y1NoZwwNBJ7zgASqa6jpMUyjEXxDwYbeIAPk+L5vvwH5fj+f7YGR2iIjI1G0TBDFfExLFsVM+7jOKko2phuI7gSQlQCJMTjPg4kuJJWpWVsOqyvqYYGiA361OatrshaIAQFRICiL53ABZZ4xBUw3DhSIHkoH5AVOsFIAJeMyWRalSDpRAtTrKFOVJRFUXAUVtT6GVoV5VVJ41QAbPVWWNR5wiupgHrugFPr+oGwahsBqCVng8KIsiqKpummaStYYYoMAAU9X15TEYNVABkGIbLUCEZppKC1UNmm0uHm1xdIWDzFqWJ5XBNt33fSvwQIQgJXFguBWLSIA6FEMD4AgrAgAAspgvrYAAIjAWR8C4-D5SAoGISD8CsPIdDSAA7C1XbCOYQA";
 
 const component = `
   <Card sx={{ my: 2, mx: [2, "auto"], p: 0, pb: 3, maxWidth: "500px" }}>
@@ -19,8 +19,8 @@ const component = `
         py: 1,
       }}
     >
-      {["Thead1", "Thead2", "Thead3"].map((h) => (
-        <Text sx={{ fontWeight: "bold" }}>{h}</Text>
+      {["Thead1", "Thead2", "Thead3"].map((h, key) => (
+        <Text sx={{ fontWeight: "bold" }} key={key}>{h}</Text>
       ))}
     </Grid>
     <Box
@@ -52,10 +52,10 @@ const component = `
         ["row4", "1", "ok"],
         ["row5", "1", "ok"],
         ["row6", "1", "ok"],
-      ].map((row) => (
-        <Grid columns={3}>
-          {row.map((cell) => (
-            <Text>{cell}</Text>
+      ].map((row, key) => (
+        <Grid columns={3} key={key}>
+          {row.map((cell, key) => (
+            <Text key={key}>{cell}</Text>
           ))}
         </Grid>
       ))}
